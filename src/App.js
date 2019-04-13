37aa1ee8a459fa6aa55dc6ea51a36f082da079a2
Shane O'Mahony <somahon@users.noreply.github.com>
2019-04-13 12:27:36 +0100

    add sample JSON data for the survey and setup the top level widget

--- src/App.js
+++ src/App.js
@@ -1,28 +1,60 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import SurveyQuestions from './data/SurveyQuestionsData';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      currentQuestionIndex : 0,
+      totalScore: 0,
+      surveyStarted: false,
+    }
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>Welcome to the Basic Survey!</h1>
+
+        {!this.state.surveyStarted ? this.showWelcomeText(): this.displayQuestion(this.state.currentQuestionIndex)}
       </div>
     );
   }
+
+  displayQuestion = (questionIdx) => {
+    return (
+        <div> SURVEY STARTED!!! </div>
+    );
+  }
+
+  startSurvey = () => {
+    //update the state, render then called again to update the view
+    this.setState({
+      surveyStarted: true
+    });
+  }
+
+  /**
+   * Show the intro and welcome text only if the survey has not started
+   * @returns {*}
+   */
+  showWelcomeText = () => {
+    return (
+        <div>
+
+          There are {SurveyQuestions.surveyQuestions.length} questions in this survey.
+          At least one answer must be given in each.
+
+          <div>
+            <button onClick={this.startSurvey}>Begin the survey!</button>
+          </div>
+        </div>
+    );
+  }
+
 }
 
 export default App;
---
+++ src/data/SurveyQuestionsData.js
@@ -0,0 +1,34 @@
+const SURVEY_QUESTIONS = {
+    "surveyQuestions": [
+        {
+            "text": "What is the capital of Ireland?",
+            "answers": [
+                "Dublin",
+                "New York",
+                "Cork"
+            ],
+            "correctAnswer": 0
+        },
+        {
+            "text":"What character was Walt Disney's first creation?",
+            "answers":[
+                "Mickey Mouse",
+                "Captain America",
+                "Shawshank Redemption"
+            ],
+            "correctAnswer": 0
+        },
+        {
+            "text":"What is 2 + 2?",
+            "answers":[
+                "22",
+                "44",
+                "4"
+            ],
+            "correctAnswer": 2
+        },
+
+    ]
+};
+
+export default SURVEY_QUESTIONS;
